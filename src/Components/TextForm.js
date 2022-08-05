ebd21163983c3778edc7e751838884aff9c5a368
Rohitsharma42 <sharmarohit9042@gmail.com>
2022-08-05 19:08:38 +0530

    Fixing issues

--- src/Components/TextForm.js
+++ src/Components/TextForm.js
@@ -37,15 +37,15 @@ export default function TextForm(props) {
              <h1>{props.heading}</h1>
             <div className="mb-3">
                 <textarea className="form-control" value={text} id="mybox" onChange={handleOnChange} style={{backgroundColor: props.mode==='dark'?'#484646':'white', color: props.mode==='dark'?'white':'black' }} rows="8"></textarea>
-                <button className="btn btn-primary mx-2 my-3"  onClick={upper}>Convert to Uppercase</button>
-                <button className="btn btn-primary mx-2 my-3"  onClick={lower}>Convert to Lowercase</button>
-                <button className="btn btn-primary mx-2 my-3"  onClick={clear}>Clear Text</button>
-                <button id= "copy" className="btn btn-primary mx-2 my-3"  onClick={copy}>Copy to Clipboard</button>
+                <button disabled={text.length===0} className="btn btn-primary mx-2 my-3"  onClick={upper}>Convert to Uppercase</button>
+                <button disabled={text.length===0} className="btn btn-primary mx-2 my-3"  onClick={lower}>Convert to Lowercase</button>
+                <button disabled={text.length===0} className="btn btn-primary mx-2 my-3"  onClick={clear}>Clear Text</button>
+                <button disabled={text.length===0} id= "copy" className="btn btn-primary mx-2 my-3"  onClick={copy}>Copy to Clipboard</button>
             </div>
         </div> 
         <div className="container" style= {{color: props.mode==='dark'?'white':'black'}}>
             <h2>Your text summary</h2>
-            <p>{text.split(" ").length} words, {text.length} characters</p>
+            <p>{text.split(" ").filter((element)=>{return element.length!==0}).length} words, {text.length} characters</p>
             <p>{0.008 * text.split("").length} Minutes read</p>
             <h2>Preview</h2>
             <p>{text.length>0?text:"Enter something in the textbox to preview it here"}</p>
